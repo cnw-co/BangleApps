334424a1deda25c44a5dfe8440a642e4b9abcb5b
glaszlo9312 <59116668+glaszlo9312@users.noreply.github.com>
2021-02-23 13:57:10 +0100

    Update boot.js

--- apps/univerz-welcome/boot.js
+++ apps/univerz-welcome/boot.js
@@ -1,8 +1,8 @@
 (function() {
-  let s = require('Storage').readJSON('welcome.json', 1) || {};
+  let s = require('Storage').readJSON('univerz-welcome.json', 1) || {};
   if (!s.welcomed) {
     setTimeout(() => {
-      require('Storage').write('welcome.json', {welcomed: true})
+      require('Storage').write('univerz-welcome.json', {welcomed: true})
       load('univerz-welcome.app.js')
     })
   }
