fc11c9d8cc5c6a517b8624e98ffe15ebb15743c6
glaszlo9312 <59116668+glaszlo9312@users.noreply.github.com>
2021-03-09 15:07:16 +0100

    further increase of btn watch delay

--- apps/univerz/app.js
+++ apps/univerz/app.js
@@ -36,7 +36,7 @@ function init() {
     //scan();
     //waitMessage();
     //setInterval(scan, BT_SCAN_INTERVAL);
-    setTimeout(() => setWatch(exit, BTN2, { repeat: false, edge: "falling" }), 500);
+    setTimeout(() => setWatch(exit, BTN2, { repeat: false, edge: "falling" }), 2000);
 }
 
 function onHRM(hrm) {
